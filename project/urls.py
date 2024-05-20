eaf712de50f842f40a3b48880a143c8625cb086c
jhin-1 <ahmedyosri52@gmail.com>
2024-05-20 15:58:52 +0300

    create user abstract for authentaication not migrations

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include(Dashboard_urls)),
+    # path("",include(Dashboard_urls)),
     path('api-auth/', include('rest_framework.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
